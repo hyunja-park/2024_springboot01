57fcca5808786cf911b7dc3a2710171bd56fe70d
parkhyunja <hjpark20121125@gmail.com>
2024-11-15 15:53:45 +0900

    세번째 올리기

--- src/main/java/com/ict/edu2/SampleController.java
+++ src/main/java/com/ict/edu2/SampleController.java
@@ -4,12 +4,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
@@ -37,7 +37,7 @@ public class SampleController {
 
     // 반환형이 객체일때
     @GetMapping("/hello4")
-    public SampleVO getHello4() {
+    public List<SampleVO> getHello4() {
         List<SampleVO> list = new ArrayList<>();
 
         SampleVO svo = new SampleVO();
@@ -46,28 +46,28 @@ public class SampleController {
         list.add(svo);
 
         SampleVO svo2 = new SampleVO();
-        svo.setName("희동이");
-        svo.setAge("3");
+        svo2.setName("희동이");
+        svo2.setAge("3");
         list.add(svo2);
 
         SampleVO svo3 = new SampleVO();
-        svo.setName("도우너");
-        svo.setAge("17");
+        svo3.setName("도우너");
+        svo3.setAge("17");
         list.add(svo3);
 
         return list;
     }
 
-    // 반환형이 객체일때
+    // 반환형이 객체 일때
     @GetMapping("/hello5")
     public DataVO getHello5(String number) {
-        DataVO datavo = new DataVO();
+        DataVO dataVO = new DataVO();
 
         List<SampleVO> list = new ArrayList<>();
 
-        SampleVO svo = new SampleVO("고길동","24");
-        SampleVO svo2 = new SampleVO("희동이","3");
-        SampleVO svo3 = new SampleVO("도우너","17");
+        SampleVO svo = new SampleVO("고길동", "54");
+        SampleVO svo2 = new SampleVO("희동이", "23");
+        SampleVO svo3 = new SampleVO("도우너", "47");
         list.add(svo);
         list.add(svo2);
         list.add(svo3);
@@ -90,6 +90,7 @@ public class SampleController {
         return DataVO.fail(0, "실패");
 
     }
+
     // 반환형이 객체 일때
     @PostMapping("/hi")
     public String getHi(String idx) {
@@ -98,8 +99,62 @@ public class SampleController {
     }
 
     @PostMapping("/hi2")
-    public String[] getHi2(String idx) {
+    public String[] getHi2(@RequestParam("idx") String idx) {
         return new String[]{"Hello", "World"};
+    }
+    
+    @PostMapping("/hi3")
+    public String[] getHi3(@RequestParam("idx") String idx) {
+        return new String[]{"Hello", "World"};
+    }
+    
+    @PostMapping("/hi4")
+    // 요청 자체가 json 방식일때
+    public String[] getHi4(@RequestBody Map<String, String> params) {
+        return new String[]{"Hello", "World"};
+    }
+
+    // 반환형이 객체일때
+    @GetMapping("/hi5")
+    public SampleVO getHi5() {
+        SampleVO svo = new SampleVO();
+        svo.setName("고길동");
+        svo.setAge("24");
+
+        return svo;
+    }
+
+    // 반환형이 객체일때
+    @PostMapping("/hi7")
+    public DataVO getHi7(@RequestParam String number) {
+       DataVO dataVO = new DataVO();
+
+        List<SampleVO> list = new ArrayList<>();
+
+        SampleVO svo = new SampleVO("고길동", "54");
+        SampleVO svo2 = new SampleVO("희동이", "23");
+        SampleVO svo3 = new SampleVO("도우너", "47");
+        list.add(svo);
+        list.add(svo2);
+        list.add(svo3);
+  
+
+        if (number.equals("1")) {
+            //dataVO.setResultCode(resultCode:1);
+            //dataVO.setMessage(message:"성공");
+            //dataVO.setData(list);
+
+            return DataVO.success(list, "성공");
+
+        } else {
+            //dataVO.setResultCode(0);
+            //dataVO.setMessage("실패");
+            //dataVO.setData(null);
+        }
+
+
+        return DataVO.fail(0, "실패");
+
     }
     
 
